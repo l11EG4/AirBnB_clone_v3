d6bd1d8b478ba8f1dd4bfba9c850aa23e50c707a
Laila <lailatabourit@gmail.com>
2023-10-27 16:22:59 -0700

    tasks done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Initialize Blueprint views"""
-"""BY LAILA AND MEGA"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """states.py"""
-"""by laila and mega"""
 
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
