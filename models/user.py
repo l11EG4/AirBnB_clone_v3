fee803d6cca4c3cc82f0bcb8bced8d64993d7772
Laila <lailatabourit@gmail.com>
2023-10-28 09:38:29 -0700

    tasks done

--- models/user.py
+++ models/user.py
@@ -27,3 +27,18 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = hashlib.md5(pwd.encode()).hexdiges    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = hashlib.md5(pwd.encode()).hexdigest()
