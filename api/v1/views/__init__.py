3367c14c85f84ee6ea78a6ea12af03c41bd9f330
Laila <lailatabourit@gmail.com>
2023-10-30 07:47:26 -0700

    tasks done

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+init file to the app routes with app_views
+"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
