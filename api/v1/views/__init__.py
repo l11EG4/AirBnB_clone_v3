39dfc8e806ec44f1b141ba64ec44fde7fb312b46
l11EG4 <zakariaberahmi@gmail.com>
2023-10-30 13:11:47 -0700

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-init file to the app routes with app_views
+init file to the app with app_views
 """
 from flask import Blueprint
 
@@ -8,3 +8,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
